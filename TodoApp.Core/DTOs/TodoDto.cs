c5390be2a4731381dcd31441ec7c6156745a9edc
Flynn Bui <kuboy26@gmail.com>
2024-06-12 13:53:23 +0700

    Add TodoDto again

--- TodoApp.Core/DTOs/TodoDto.cs
+++ TodoApp.Core/DTOs/TodoDto.cs
@@ -1,3 +1,25 @@
-﻿namespace TodoApp.Core.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
 
-public record TodoDto();
+namespace TodoApp.Core.DTOs;
+
+public record class CreateTodoDto(
+    [Required]
+    [StringLength(50)]
+    string Title,
+
+    string? Description
+);
+
+public record class UpdateTodoDto(
+    [Required]
+    int Id,
+
+    [Required]
+    [StringLength(50)]
+    string Title,
+
+    [StringLength(1000)]
+    string Description,
+
+    bool Status
+);
